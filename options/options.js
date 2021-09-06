34a91289a9ade2e94354ac6ef9a3ed629781ac12
Crowdbotics <team@crowdbotics.com>
2021-09-06 22:08:09 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,6 @@
+export const globalOptions = {
+  name: "curly_hat_30364Identifier",
+  url: "https://curly_hat_30364Identifier.botics.co",
+  api: "https://curly_hat_30364Identifier.botics.co/api/v1"
+}
+export const modulesOptions = {}
